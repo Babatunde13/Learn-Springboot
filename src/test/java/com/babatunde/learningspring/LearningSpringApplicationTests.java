ce89562100f35ea0ce248ffbf0edfcccbd0429d8
Babatunde Koiki <koikibabatunde14@gmail.com>
2023-01-04 14:44:30 +0100

    Initial commit

---
+++ src/test/java/com/babatunde/learningspring/LearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babatunde.learningspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
