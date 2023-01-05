39ccf1c6032b4aea638cabcda060e779a75f7aa9
Babatunde Koiki <koikibabatunde14@gmail.com>
2023-01-05 11:40:04 +0100

    updated the email regex

--- src/main/java/com/babatunde/CustomValidation.java
+++ src/main/java/com/babatunde/CustomValidation.java
@@ -4,7 +4,7 @@ import java.util.regex.*;;
 
 public class CustomValidation {
     public static boolean isEmailValid(String email) {
-        Pattern pattern = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+        Pattern pattern = Pattern.compile("^[a-z0-9._-]+@[a-z0-9-]+(\\.[a-z]{2,6}){1,2}$");
         return pattern.matcher(email).find();
     }
 
@@ -14,7 +14,7 @@ public class CustomValidation {
     }
 
     public static boolean isPasswordValid(String password) {
-        Pattern pattern = Pattern.compile("^[a-z0-9@.-]{8,20}$", Pattern.CASE_INSENSITIVE);
+        Pattern pattern = Pattern.compile("^[\\w@-]{8,20}$", Pattern.CASE_INSENSITIVE);
         return pattern.matcher(password).find();
     }
 
@@ -34,6 +34,8 @@ public class CustomValidation {
     }
 
     public static void main(String[] args) {
+        System.out.println(isEmailValid("koikibabatunde14@gmail.co.uk"));
         System.out.println(isEmailValid("koikibabatunde14@gmail.com"));
+        System.out.println(isEmailValid("koikibabatunde14@gmail.co.uk.uk"));
     }
 }
